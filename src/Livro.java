aed24cc491a56320a1b79bcf2bdb5d4ca2c8ffd5
edu64123 <eduardonerykurahara@gmail.com>
2026-03-12 09:47:24 -0300

    Classe livro e criação da classe testo com o objeto meuLivro

---
+++ src/Livro.java
@@ -0,0 +1,14 @@
+import java.util.StringTokenizer;
+
+public class Livro {
+    //Atributos: Características do objeto
+    //tipo nomeAtributo;
+    String titulo;
+    String autor;
+    double preco;
+    String editora;
+    int paginas;
+    String resumo;
+    String tipoCapa;
+}
+
---
+++ src/TesteLivro.java
@@ -0,0 +1,8 @@
+public class TesteLivro {
+    public static void main(String[] args) {
+        //Criar objetos - instanciar
+        Livro meuLivro = new Livro();
+        meuLivro.titulo = "Harry potter";
+        meuLivro.editora = "editora";
+    }
+}
